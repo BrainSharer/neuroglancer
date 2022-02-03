bc836760397d80e7a3d102244e4a7d9847d27a70
Austin Hoag <austinthomashoag@gmail.com>
2022-02-03 16:10:18 -0500

    add field displaying recent fetch results

--- src/neuroglancer/widget/fetch_tracing_annotation.ts
+++ src/neuroglancer/widget/fetch_tracing_annotation.ts
@@ -1,4 +1,4 @@
-import './fetch_annotation.css';
+import './fetch_tracing_annotation.css';
 import { StatusMessage } from 'neuroglancer/status';
 import { RefCounted } from 'neuroglancer/util/disposable';
 import { removeFromParent } from 'neuroglancer/util/dom';
@@ -21,6 +21,7 @@ export class FetchTracingAnnotationWidget extends RefCounted {
   private primaryInjectionSite: HTMLSelectElement;
 
   // private addAnnotationButton: HTMLElement;
+  public lastFetchField: HTMLElement;
   private fetchButton: HTMLElement;
   // private numberNeuronsShownField: HTMLElement
 
@@ -39,7 +40,7 @@ export class FetchTracingAnnotationWidget extends RefCounted {
     // Title for the tool
     const fetchTitle = document.createElement('h3');
     fetchTitle.innerText = "Load Viral Tracing annotations"
-    fetchTitle.classList.add('neuroglancer-mouselight-tool-title')
+    fetchTitle.classList.add('neuroglancer-tracing-tool-title')
 
     ///////// FILTER #1 /////////
     // Make the div for this fitler (i.e. set of fields)
@@ -47,8 +48,8 @@ export class FetchTracingAnnotationWidget extends RefCounted {
     
     // Virus timepoint -- e.g. "HSV-H129_Disynaptic" ,
     this.virusTimepoint = document.createElement('select');
-    this.virusTimepoint.id = "mouselight-filter-type-1";
-    this.virusTimepoint.classList.add('neuroglancer-fetch-mouselight-selection');
+    this.virusTimepoint.id = "tracing-filter-type-1";
+    this.virusTimepoint.classList.add('neuroglancer-fetch-tracing-selection');
    
     const defaultOptionVirusTimepoint = document.createElement('option');
     defaultOptionVirusTimepoint.text = 'Select Virus and Timepoint';
@@ -66,7 +67,7 @@ export class FetchTracingAnnotationWidget extends RefCounted {
 
     // Primary injection site
     this.primaryInjectionSite = document.createElement('select');
-    this.primaryInjectionSite.classList.add('neuroglancer-fetch-mouselight-selection');
+    this.primaryInjectionSite.classList.add('neuroglancer-fetch-tracing-selection');
 
     const defaultOptionPrimaryInjectionSite = document.createElement('option');
     defaultOptionPrimaryInjectionSite.text = 'Select Primary Injection Site';
@@ -94,17 +95,17 @@ export class FetchTracingAnnotationWidget extends RefCounted {
       onClick: () => {this.fetchTracingBrains()},
     });
     
-    this.fetchButton.classList.add('neuroglancer-fetch-mouselight-button');
+    this.fetchButton.classList.add('neuroglancer-fetch-tracing-button');
 
-    // // Text area showing how many tracing brains were fetched are shown
-    // this.numberNeuronsShownField = document.createElement('p');
-    // this.numberNeuronsShownField.innerHTML = '';
-    // this.numberNeuronsShownField.classList.add('neuroglancer-mouselight-filter-title');
+    // // Text area showing how many tracing brains were fetched and what the fetch criteria were
+    this.lastFetchField = document.createElement('p');
+    this.lastFetchField.innerHTML = '';
+    this.lastFetchField.classList.add('neuroglancer-tracing-filter-title');
 
     // Now add all child elements to parent filter 
     this.element.appendChild(filterField);
     this.element.appendChild(this.fetchButton);
-    // this.element.appendChild(this.numberNeuronsShownField);
+    this.element.appendChild(this.lastFetchField);
     this.registerDisposer(() => removeFromParent(this.element));
   }
  
@@ -137,8 +138,18 @@ export class FetchTracingAnnotationWidget extends RefCounted {
           return response.json();
         });
 
-        const brain_names = tracingJSON.brain_names;
+        // const brain_names = tracingJSON.brain_names;
         const n_brains_fetched = tracingJSON.brain_urls.length;
+        this.lastFetchField.innerHTML = `Last fetch: <br />
+        Virus/Timepoint: ${virusTimepoint}<br />
+        Primary Injection Site: ${primaryInjectionSite}<br />
+        Number of brains fetched: ${n_brains_fetched}`;
+        if (n_brains_fetched == 0) {
+          this.lastFetchField.style.color = 'red';
+        }
+        else {
+          this.lastFetchField.style.color = 'white';
+        }
         if (n_brains_fetched > 10) {
           StatusMessage.showTemporaryMessage('More than 10 brains would be fetched. Please adjust your search to fetch 10 or fewer');
           throw("Error. Too many brains would be fetched. Please adjust your search to fetch 10 or fewer");
@@ -146,27 +157,32 @@ export class FetchTracingAnnotationWidget extends RefCounted {
 
         let brain_counter = 0;        
         tracingJSON.brain_urls.forEach((precomputed_url:string) => {
-          const brain_name = brain_names[brain_counter]
+          // const brain_name = brain_names[brain_counter]
+          const layer_name = virusTimepoint + ' PRI_INJ: ' + primaryInjectionSite + ' ' + '(' + String(brain_counter) + ')'; 
           // Get list of layer names 
           const layerSet = this.layer.manager.layerManager.layerSet;
           let layerNameArray = new Array<string>();
           layerSet.forEach((entry:any) => {
               layerNameArray.push(entry.name_)
             });
-          console.log(layerNameArray);
-          console.log(brain_name);
 
           // Only add new layer if layer not in layer list
 
-          if (!layerNameArray.includes(brain_name)) {
+          if (!layerNameArray.includes(layer_name)) {
             // pick a random color for the annotations
             let randomColor = Math.floor(Math.random()*16777215).toString(16);
             let hex_color =  '#' + randomColor;
+            // Disable all layers except first 
+            let visible = true;
+            if (brain_counter > 0) {
+              visible = false
+            }
             const newLayer = makeLayer(
-              this.layer.manager, brain_name,
+              this.layer.manager, layer_name,
                 {type: 'annotation',
                  'source': 'precomputed://' + precomputed_url,
                  'annotationColor': hex_color,
+                 'visible': visible,
                  'shaderControls': {
                    "size":2.0,
                    "opacity":0.75}
@@ -175,7 +191,7 @@ export class FetchTracingAnnotationWidget extends RefCounted {
             this.layer.manager.add(newLayer);
           }
           else {
-            StatusMessage.showTemporaryMessage(`Brain ${brain_name} is already loaded`);
+            StatusMessage.showTemporaryMessage(`Layer: ${layer_name} is already loaded`);
             }
           brain_counter+=1;
         });
