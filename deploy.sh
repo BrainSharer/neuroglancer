0d5ec2d5b0d5cbab0e096336a160d22102e1682a
Litao Qiao <checkpppp@gmail.com>
2024-02-12 02:49:12 -0800

    update deploy scripts

--- deploy.sh
+++ deploy.sh
@@ -1,5 +1,43 @@
 #!/bin/bash
 
+SERVICE_PATH="./src/brainshare/service.ts" # Update this path to your file's actual path
+SERVICE_PRODUCTION=$(cat <<'EOF'
+export const APIs = {
+  IMAGESERVER_API_ENDPOINT: 'https://imageserv.dk.ucsd.edu/brainsharer',
+  API_ENDPOINT: 'https://www.brainsharer.org/brainsharer',
+  GOOGLE_LOGIN: 'https://www.brainsharer.org/brainsharer/accounts/google/login/?next=',
+  LOCAL_LOGIN: 'https://www.brainsharer.org/brainsharer/admin/login/?next=',
+  LOGOUT: 'https://www.brainsharer.org/brainsharer/local/logout/',
+  ADMIN_PORTAL: 'https://www.brainsharer.org/brainsharer/admin/',
+  REFRESH_TOKEN: 'https://www.brainsharer.org/brainsharer/api-token-refresh/',
+};
+EOF
+)
+SERVICE_DEMO=$(cat <<'EOF'
+export const APIs = {
+  IMAGESERVER_API_ENDPOINT: 'https://imageserv.dk.ucsd.edu/brainsharer',
+  API_ENDPOINT: 'https://demo.brainsharer.org/brainsharer',
+  GOOGLE_LOGIN: 'https://demo.brainsharer.org/brainsharer/accounts/google/login/?next=',
+  LOCAL_LOGIN: 'https://demo.brainsharer.org/brainsharer/admin/login/?next=',
+  LOGOUT: 'https://demo.brainsharer.org/local/logout',
+  ADMIN_PORTAL: 'https://demo.brainsharer.org/brainsharer/admin/',
+  REFRESH_TOKEN: 'https://demo.brainsharer.org/brainsharer/api-token-refresh/',
+};
+EOF
+)
+SERVICE_LOCAL=$(cat <<'EOF'
+export const APIs = {
+  IMAGESERVER_API_ENDPOINT: 'https://imageserv.dk.ucsd.edu/brainsharer',
+  API_ENDPOINT: 'http://localhost:8000',
+  GOOGLE_LOGIN: 'http://localhost:8000/google/',
+  LOCAL_LOGIN: 'http://localhost:8000/local/login',
+  LOGOUT: 'http://localhost:8000/local/logout',
+  ADMIN_PORTAL: 'https://localhost:8000/admin/',
+  REFRESH_TOKEN: 'http://localhost:8000/api-token-refresh/',
+};
+EOF
+)
+
 if [ "$1" == "" ] || [ $# -gt 1 ]; then
     echo "Enter either 'production' or 'demo' as an argument."
 	exit 0
@@ -15,15 +53,15 @@ rm -vf *.tar.gz
 GIT=$(git tag --sort=version:refname)
 
 if [ "$1" == "demo" ]; then
-    BUILD="build-demo"
     BUILD_INFO="{'tag':'DEMO Version $GIT', 'url':'https://github.com/BrainSharer/neuroglancer/commit/$(git rev-parse HEAD)', 'timestamp':'$(date)'}"
     PACKAGE="neuroglancer.demo.tar.gz"
+    echo "$SERVICE_DEMO" > "$SERVICE_PATH"
 fi
 
 if [ "$1" == "production" ]; then
-    BUILD="build-min"
     BUILD_INFO="{'tag':'Production Version $GIT', 'url':'https://github.com/BrainSharer/neuroglancer/commit/$(git rev-parse HEAD)', 'timestamp':'$(date)'}"
     PACKAGE="neuroglancer.production.tar.gz"
+    echo "$SERVICE_PRODUCTION" > "$SERVICE_PATH"
 fi
 
 if ! command -v npm &> /dev/null
@@ -32,12 +70,13 @@ then
     exit 1
 fi
 
-echo $BUILD
 echo "$BUILD_INFO"
 # exit 0
 
 #npm run build-python -- --no-typecheck --define NEUROGLANCER_BUILD_INFO="${BUILD_INFO}"
-npm run $BUILD -- --define NEUROGLANCER_BUILD_INFO="${BUILD_INFO}"
+npm run build -- --define NEUROGLANCER_BUILD_INFO="${BUILD_INFO}"
 cd dist/min/
 tar zcvf ../../$PACKAGE *
 cd ../../
+
+echo "$SERVICE_LOCAL" > "$SERVICE_PATH"
--- src/brainshare/service.ts
+++ src/brainshare/service.ts
@@ -1,31 +1,9 @@
-/**
- * @license
- * Copyright 2018 Google Inc.
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * Modified for Brainsharer UCSD/Princeton
- */
-
-/**
- * @file Settings for local server support in testing.
- */
-
 export const APIs = {
-  API_ENDPOINT: 'http://localhost:8000',
   IMAGESERVER_API_ENDPOINT: 'https://imageserv.dk.ucsd.edu/brainsharer',
+  API_ENDPOINT: 'http://localhost:8000',
   GOOGLE_LOGIN: 'http://localhost:8000/google/',
   LOCAL_LOGIN: 'http://localhost:8000/local/login',
   LOGOUT: 'http://localhost:8000/local/logout',
   ADMIN_PORTAL: 'https://localhost:8000/admin/',
   REFRESH_TOKEN: 'http://localhost:8000/api-token-refresh/',
 };
-				  
